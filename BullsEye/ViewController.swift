798ba9ef99f338ea0d7d3aa25e2b7ef1ac347ad3
Armno <monkeyarmno@gmail.com>
2015-11-30 20:55:38 +0700

    finish game functionalities

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-  var currentValue: Int = 50
+  var currentValue = 0
+  var targetValue = 0
+  var score = 0
+  var round = 0
+  
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var targetLabel: UILabel!
+  @IBOutlet weak var scoreLabel: UILabel!
+  @IBOutlet weak var roundLabel: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    startNewRound()
+    updateLabels()
   }
 
   override func didReceiveMemoryWarning() {
@@ -24,7 +33,12 @@ class ViewController: UIViewController {
   
   @IBAction func showAlert() {
     
-    let message = "The value of the slider is: \(currentValue)"
+    let difference = abs(currentValue - targetValue)
+    let points = 100 - difference
+    
+    score += points
+    
+    let message = "Your score is: \(points) points"
     
     let alert = UIAlertController(title: "Hello wolrd", message: message, preferredStyle: .Alert)
     
@@ -34,10 +48,26 @@ class ViewController: UIViewController {
     
     presentViewController(alert, animated: true, completion: nil)
     
+    startNewRound()
+    updateLabels()
+    
   }
   
   @IBAction func sliderMoved(slider: UISlider) {
     currentValue = lroundf(slider.value)
+  }
+  
+  func startNewRound() {
+    round += 1
+    targetValue = 1 + Int(arc4random_uniform(100))
+    currentValue = 50
+    slider.value = Float(currentValue)
+  }
+  
+  func updateLabels() {
+    targetLabel.text = String(targetValue)
+    scoreLabel.text = String(score)
+    roundLabel.text = String(round)
   }
 
 
