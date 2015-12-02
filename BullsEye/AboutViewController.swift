ec811c321d623c148aaf1d001104ff9d787783b3
Armno <monkeyarmno@gmail.com>
2015-12-02 22:12:45 +0700

    create about page

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Armno Prommarak on 12/1/2558 BE.
+//  Copyright © 2558 armno. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+  
+  @IBAction func close() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+
+}
